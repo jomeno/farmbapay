dcf5f08b63c615a68d9b22dad5b5a36bfcf83909
Jomeno <jomeno.vona@gmail.com>
2025-10-25 22:06:47 -0600

    Update key

--- src/lib/google-client.ts
+++ src/lib/google-client.ts
@@ -3,12 +3,15 @@ import { google } from "googleapis"
 export function getGoogle(){
     return google;
 }
+
+const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 const options = { 
     email: process.env.CLIENT_EMAIL,
-    key: process.env.PRIVATE_KEY,
+    key: privateKey,
     scopes: ['https://www.googleapis.com/auth/spreadsheets']
 }
 export function getGoogleClient(){
+
     const client = new google.auth.JWT(options);
     return client
 }
